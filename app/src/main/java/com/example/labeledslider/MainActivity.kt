115d014e7aa8bd4329f84f983227cae75f780dd9
Nuriye <nuriyeerdogan38@gmail.com>
2025-03-08 12:05:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/labeledslider/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.labeledslider
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.labeledslider.ui.theme.LabeledSliderTheme
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Slider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.setValue
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LabeledSliderTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding).padding(16.dp)
+                    ){
+                   LabelSlider()
+                   }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LabelSlider(modifier:  Modifier = Modifier) {
+    var sliderPosition by remember { mutableStateOf(0f) }
+
+    Column (
+        modifier = modifier.padding(8.dp)
+    ) {
+
+        Slider(
+            value = sliderPosition,
+            onValueChange = {  sliderPosition = it },
+            valueRange = 0f..100f,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = "Slider Value: ${sliderPosition.toInt()}", modifier = Modifier.padding(top = 8.dp))
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LabelSliderPreview() {
+    LabeledSliderTheme {
+        LabelSlider()
+    }
+}
